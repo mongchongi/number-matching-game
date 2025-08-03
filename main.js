00ec50693210b44de88e219fba5f4e6e707ff221
Yeonguk Bak <yeonguk0517@gmail.com>
2025-08-03 16:25:20 +0900

    입력한 값에 따라 힌트 보여 주기

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
+const hintMessage = document.querySelector('.hint-message');
+const numberField = document.querySelector('.answer-input__number-field');
+const confirmButton = document.querySelector('.answer-input__confirm-button');
+
 let randomAnswer = 0;
 
 function generateRandomNumber() {
   randomAnswer = Math.floor(Math.random() * 100) + 1;
+  console.log('🚀 ~ generateRandomNumber ~ randomAnswer:', randomAnswer);
+}
+
+function handlePlayGame() {
+  const userAnswer = numberField.value;
+
+  if (userAnswer > randomAnswer) {
+    hintMessage.classList.remove('hint-message--up');
+    hintMessage.classList.add('hint-message--down');
+    hintMessage.textContent = 'DOWN! 더 작은 숫자입니다!';
+  } else if (userAnswer < randomAnswer) {
+    hintMessage.classList.remove('hint-message--down');
+    hintMessage.classList.add('hint-message--up');
+    hintMessage.textContent = 'DOWN! 더 큰 숫자입니다!';
+  }
+
+  numberField.value = '';
+  numberField.focus();
 }
 
 generateRandomNumber();
+
+confirmButton.addEventListener('click', handlePlayGame);
