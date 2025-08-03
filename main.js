d04c17e140497550ea91d3d95413f7e001e54b10
Yeonguk Bak <yeonguk0517@gmail.com>
2025-08-03 16:27:09 +0900

    입력할 때마다 기회 줄이기

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+const chanceRemainingCount = document.querySelector('.chance__remaining-count');
 const hintMessage = document.querySelector('.hint-message');
 const numberField = document.querySelector('.answer-input__number-field');
 const confirmButton = document.querySelector('.answer-input__confirm-button');
 
 let randomAnswer = 0;
+let chanceCount = 5;
 
 function generateRandomNumber() {
   randomAnswer = Math.floor(Math.random() * 100) + 1;
@@ -11,6 +13,9 @@ function generateRandomNumber() {
 
 function handlePlayGame() {
   const userAnswer = numberField.value;
+
+  chanceCount--;
+  chanceRemainingCount.textContent = chanceCount;
 
   if (userAnswer > randomAnswer) {
     hintMessage.classList.remove('hint-message--up');
